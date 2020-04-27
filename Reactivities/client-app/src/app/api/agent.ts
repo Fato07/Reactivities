1ee3cbe60aa29e6204c7ad9ba697c4ac1f340436
fadosu <fadosu@ttu.ee>
2020-04-27 15:43:06 +0300

    added routes

---
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -0,0 +1,30 @@
+import { IActivity } from './../models/activity';
+import axios, { AxiosResponse } from 'axios';
+import { setTimeout } from 'timers';
+
+
+axios.defaults.baseURL = 'https://localhost:5001/api';
+
+const responseBody = (response: AxiosResponse) => response.data;
+const sleep = (ms: number) => (response: AxiosResponse) => 
+new Promise<AxiosResponse>(resolve=> setTimeout(()=> resolve(response), ms))
+
+const requests = {
+    get: (url: string) => axios.get(url).then(sleep(800)).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(sleep(800)).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(sleep(800)).then(responseBody),
+    del: (url: string) => axios.delete(url).then(sleep(800)).then(responseBody)
+}
+
+const Activities = {
+    list: (): Promise<IActivity[]> => requests.get('/activities'),
+    details: (actvityId: string) => requests.get(`/activities/${actvityId}`),
+    create: (activity: IActivity) => requests.post('/activities', activity),
+    update: (activity: IActivity) => requests.put(`/activities/${activity.activityId}`, activity),
+    delete: (actvityId: string) => requests.del(`/activities/${actvityId}`),
+
+}
+
+export default {
+    Activities
+}
