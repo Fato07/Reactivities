add30ee90074de3b0d425460ffc60f551893cc21
fadosu <fadosu@ttu.ee>
2020-05-16 21:05:33 +0300

    Major refactorings and added Page sorting

---
+++ Reactivities/client-app/src/features/profiles/ProfileContent.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import { ProfilePhotos } from './ProfilePhotos'
+import ProfileDescription from './ProfileDescription'
+import ProfileFollowings from './ProfileFollowings'
+
+//An Array of Objects which has a menuItem fopr the Content of the Page
+const panes = [
+    {menuItem: 'About', render: () => <ProfileDescription/> },
+    {menuItem: 'Photos', render: () => <ProfilePhotos/> },
+    {menuItem: 'Activities', render: () => <Tab.Pane>Activities Content</Tab.Pane> },
+    {menuItem: 'Followers', render: () => <ProfileFollowings/>},
+    {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane> }
+]
+
+const ProfileContent = () => {
+    return (
+        <Tab
+        menu={{fluid: true, vertical: true}}
+        menuPosition='right'
+        panes={panes}
+        />
+    )
+}
+
+export default ProfileContent;
