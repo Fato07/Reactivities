66946e7d983c819d3d8432288c500419924eef08
fadosu <fadosu@ttu.ee>
2020-05-18 16:28:25 +0300

    Serving Production App From API Server

---
+++ Reactivities/WebAPI/wwwroot/precache-manifest.689958f92490049b57f2a5be4f9548bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e503481349f855ed35da3b47252bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d71716f98a8faa5e71f3",
+    "url": "/static/css/2.ed207544.chunk.css"
+  },
+  {
+    "revision": "041bfe47db2b0aad4636",
+    "url": "/static/css/main.8a6cd4ba.chunk.css"
+  },
+  {
+    "revision": "d71716f98a8faa5e71f3",
+    "url": "/static/js/2.08dab54b.chunk.js"
+  },
+  {
+    "revision": "c3129c3866644af74b85e5813b34daf4",
+    "url": "/static/js/2.08dab54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041bfe47db2b0aad4636",
+    "url": "/static/js/main.c43ccf9b.chunk.js"
+  },
+  {
+    "revision": "db9f067e02a9ca36140d",
+    "url": "/static/js/runtime-main.37884cf8.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
